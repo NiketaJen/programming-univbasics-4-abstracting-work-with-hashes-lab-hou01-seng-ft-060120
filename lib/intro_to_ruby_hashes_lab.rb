01381b0f2e3480c1614ee5cafda5174252977ffb
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-11 06:25:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def my_hash_creator(key, value)
-  my_hash_creator=hash.new # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator=Hash.new
 end
 
 def read_from_hash(hash, key)
